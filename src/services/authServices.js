7d07bd284ccfca7f39b46ebe7fd6507755c72156
Memyer <153522750+Memyer@users.noreply.github.com>
2024-06-14 02:18:46 +0700

    edit services

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,50 +1,73 @@
-const Firestore = require("@google-cloud/firestore");
+const { Firestore } = require("@google-cloud/firestore");
 
-
+// Initialize Firestore with the specified database
 const db = new Firestore({
   databaseId: "formfitdbs",
 });
+
+// Reference to the users collection in Firestore
 const usersCollection = db.collection("users");
 
-const getUserByEmail = async (email) => {
-  return await usersCollection.where("email", "==", email).get();
+/**
+ * Retrieve a user by their email address.
+ * @param {string} email - The email address of the user.
+ * @returns {Promise<Firestore.QuerySnapshot>} - A promise that resolves to the query snapshot of the user.
+ */
+const findUserByEmail = async (email) => {
+  return usersCollection.where("email", "==", email).get();
 };
 
-
-const createUser = (userData) => {
-  return db.collection("users").add(userData);
+/**
+ * Add a new user to the Firestore database.
+ * @param {Object} userData - The user data to add.
+ * @returns {Promise<Firestore.DocumentReference>} - A promise that resolves to the document reference of the new user.
+ */
+const addUser = (userData) => {
+  return usersCollection.add(userData);
 };
 
-
-const updateUserById = async (id, user) => {
+/**
+ * Update user information by their document ID.
+ * @param {string} id - The document ID of the user.
+ * @param {Object} user - The new user data to update.
+ * @returns {Promise<Firestore.WriteResult>} - A promise that resolves to the write result of the update.
+ */
+const modifyUserById = async (id, user) => {
   const userDoc = usersCollection.doc(id);
   const updateData = {};
 
   if (user.email !== undefined) {
     updateData.email = user.email;
   }
-
   if (user.password !== undefined) {
     updateData.password = user.password;
   }
-
   if (user.name !== undefined) {
     updateData.name = user.name;
   }
 
-  return await userDoc.update(updateData);
+  return userDoc.update(updateData);
 };
 
-
-const deleteUserById = async (id) => {
+/**
+ * Delete a user from the Firestore database by their document ID.
+ * @param {string} id - The document ID of the user.
+ * @returns {Promise<Firestore.WriteResult>} - A promise that resolves to the write result of the delete operation.
+ */
+const removeUserById = async (id) => {
   const userDoc = usersCollection.doc(id);
-  return await userDoc.delete();
+  return userDoc.delete();
 };
 
-
-const getUserById = async (id) => {
+/**
+ * Retrieve a user by their document ID.
+ * @param {string} id - The document ID of the user.
+ * @returns {Promise<Firestore.DocumentSnapshot>} - A promise that resolves to the document snapshot of the user.
+ */
+const findUserById = async (id) => {
   const userDoc = usersCollection.doc(id);
-  return await userDoc.get();
+  return userDoc.get();
 };
 
-module.exports = { getUserByEmail, createUser, updateUserById, deleteUserById, getUserById };
+// Export the functions for use in other modules
+module.exports = { findUserByEmail, addUser, modifyUserById, removeUserById, findUserById };
