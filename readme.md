62db0fbbcca1ab7e5ffdfb800f7fc96d38f4b9f8
Memyer <153522750+Memyer@users.noreply.github.com>
2024-06-14 00:59:55 +0700

    edit

--- readme.md
+++ readme.md
@@ -1,4 +1,1 @@
-Step : 
-1. Clone
-2. Authentikasi gcloud CLI (gcloud auth application-default login)
-3. npm install
+Backend Formfit API
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,47 +1,62 @@
+// Import necessary handlers and middleware
 const { register, login, updateUser, deleteUser, getUser } = require("../server/userHandler");
 const { verifyToken } = require("../middleware/authMiddleware");
-const { getHistory } = require("../server/formfitHandler");
 
+// Define the routes array with various endpoints
 const routes = [
+  // Test route for checking the server
+  {
+    path: '/test',
+    method: 'GET',
+    handler: (request, h) => {
+        const response = h.response({
+            status: 'success',
+            message: 'testing',
+        });
+        response.code(200); // Set HTTP status code to 200
+        return response; // Return the response object
+    }
+  },
+  // Route for user registration
   {
     method: "POST",
     path: "/register",
-    handler: register,
+    handler: register, // Handler function for user registration
   },
+  // Route for user login
   {
     method: "POST",
     path: "/login",
-    handler: login,
+    handler: login, // Handler function for user login
   },
+  // Route for updating user information
   {
     method: "PUT",
-    path: "/users/{id}",
-    handler: updateUser,
+    path: "/users/{id}", // {id} is a path parameter
+    handler: updateUser, // Handler function for updating user
     options: {
-      pre: [{ method: verifyToken }],
+      pre: [{ method: verifyToken }], // Middleware to verify token before updating user
     },
   },
+  // Route for getting user information
+  {
+    method: "GET",
+    path: "/users/{id}", // {id} is a path parameter
+    handler: getUser, // Handler function for getting user information
+    options: {
+      pre: [{ method: verifyToken }], // Middleware to verify token before fetching user
+    },
+  },
+  // Route for deleting a user
   {
     method: "DELETE",
-    path: "/users/{id}",
-    handler: deleteUser,
+    path: "/users/{id}", // {id} is a path parameter
+    handler: deleteUser, // Handler function for deleting user
     options: {
-      pre: [{ method: verifyToken }],
+      pre: [{ method: verifyToken }], // Middleware to verify token before deleting user
     },
-  },
-  {
-    method: "GET",
-    path: "/users/{id}",
-    handler: getUser,
-    options: {
-      pre: [{ method: verifyToken }],
-    },
-  },
-  {
-    path: "/history",
-    method: "GET",
-    handler: getHistory,
   },
 ];
 
+// Export the routes array to be used in server setup
 module.exports = routes;
